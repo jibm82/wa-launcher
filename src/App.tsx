a973fb205326cce059f9f94782636eed89ce42d2
Jorge Báez <jibm_82@yahoo.com.mx>
2022-08-13 12:24:07 -0500

    Setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,92 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('')
 
-  let buttonKeys = [];
+    let buttonKeys = []
 
-  for(let i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 9; i++) {
+        buttonKeys.push({
+            type: 'number',
+            value: i.toString(),
+        })
+    }
+
     buttonKeys.push({
-      type: 'number',
-      value: i.toString()
-    });
-  }
+        type: 'spacer',
+    })
 
-  buttonKeys.push({
-    type: 'spacer',
-  });
+    buttonKeys.push({
+        type: 'number',
+        value: '0',
+    })
 
-  buttonKeys.push({
-    type: 'number',
-    value: '0',
-  });
+    buttonKeys.push({
+        type: 'delete',
+    })
 
-  buttonKeys.push({
-    type: 'delete',
-  });
+    const addNumber = (number: string): void => {
+        setPhoneNumber(phoneNumber + number)
+    }
 
-  const addNumber = (number: string): void => {
-    setPhoneNumber(phoneNumber + number);
-  }
+    const removeNumber = (): void => {
+        setPhoneNumber(phoneNumber.slice(0, -1))
+    }
 
-  return (
-    <div className='h-screen p-3'>
-      <div className="card w-100 h-full">
-        <div className="card-body">
-        <input type="text" placeholder="Type a phone number" className="input input-bordered w-full max-w-xs" value={phoneNumber}/>
-          <div className="grid grid-cols-3 gap-2">
-            {buttonKeys.map((buttonKey) => (<div>
-              {buttonKey.type === 'number' && (<button className="btn btn-primary w-full" disabled={phoneNumber.length >= 10} onClick={(e) => addNumber(buttonKey.value!)}>
-                {buttonKey.value}
-              </button>)}
+    const chat = (): void => {
+        window.location.href = 'https://wa.me/52' + phoneNumber
+    }
 
-            </div>))}
-          </div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary w-full" disabled={phoneNumber.length !== 10}>Chat</button>
-          </div>
+    return (
+        <div className="h-screen p-3">
+            <div className="card w-100 h-full">
+                <div className="card-body">
+                    <input
+                        type="text"
+                        placeholder="Type a phone number"
+                        className="input input-bordered w-full max-w-xs"
+                        value={phoneNumber}
+                    />
+                    <div className="grid grid-cols-3 gap-2">
+                        {buttonKeys.map((buttonKey) => (
+                            <div>
+                                {buttonKey.type === 'number' && (
+                                    <button
+                                        className="btn btn-primary w-full"
+                                        disabled={phoneNumber.length >= 10}
+                                        onClick={() =>
+                                            addNumber(buttonKey.value!)
+                                        }
+                                    >
+                                        {buttonKey.value}
+                                    </button>
+                                )}
+                                {buttonKey.type === 'delete' && (
+                                    <button
+                                        className="btn btn-primary w-full"
+                                        disabled={phoneNumber.length === 0}
+                                        onClick={() => removeNumber()}
+                                    >
+                                        {'<<'}
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="card-actions justify-end">
+                        <button
+                            className="btn btn-primary w-full"
+                            disabled={phoneNumber.length !== 10}
+                            onClick={() => chat()}
+                        >
+                            Chat
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
